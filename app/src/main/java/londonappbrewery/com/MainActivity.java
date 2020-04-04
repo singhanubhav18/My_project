adb3f5ba0bf8a0f644014213e93390b63cbf6278
219230 <singhanubhav18>
2020-04-04 15:56:55 +0530

    Try!

--- app/src/main/java/londonappbrewery/com/MainActivity.java
+++ app/src/main/java/londonappbrewery/com/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,15 +20,26 @@ public class MainActivity extends AppCompatActivity {
         Button rollButton;
         rollButton = (Button) findViewById(R.id.rollbutton);
 
-        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
 
-        ImageView rightDice= (ImageView) findViewById(R.id.image_right_Dice);
+        final ImageView rightDice= (ImageView) findViewById(R.id.image_right_Dice);
 
+        final int[] diceArray ={ R.drawable.dice1,R.drawable.dice2,R.drawable.dice3,R.drawable.dice4,R.drawable.dice5,R.drawable.dice6};
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Log.d("Dicee","Ze Button has been pressed!");
+
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+                Log.d("Dicee" ,"The random number is: " + number );
+
+                leftDice.setImageResource(diceArray[number]);
+
+                number = randomNumberGenerator.nextInt(6);
+
+                rightDice.setImageResource(diceArray[number]);
             }
         });
     }
