c2958c128fe8ce2cd91f610bb562fceb81350314
219230 <singhanubhav18>
2020-04-04 15:29:18 +0530

    hello,

---
+++ app/src/main/java/londonappbrewery/com/MainActivity.java
@@ -0,0 +1,33 @@
+package londonappbrewery.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollbutton);
+
+        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+
+        ImageView rightDice= (ImageView) findViewById(R.id.image_right_Dice);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("Dicee","Ze Button has been pressed!");
+            }
+        });
+    }
+}
